4ea084e8fd6ac7d633c580c017f2c370452e3e11
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-01 22:12:04 +0300

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -13,54 +13,20 @@ local distance4and2 = 4.2
 
 ---- Props
 local coords = LocalPlayer:GetCoords()
-local Prop1 = Object:new('prop_shuttering04', coords+vec3(distance4, 0, -.5))
+local Prop1 = Object:new('prop_shuttering04', coords+vec3(distance4, 0, -.7))
 Prop1:Spawn()
 Prop1:SetRotation(vec3(0, 0, 180))
 
-local Prop2 = Object:new('prop_shuttering04', coords-vec3(distance4, 0, .5))
+local Prop2 = Object:new('prop_shuttering04', coords-vec3(distance4, 0, .7))
 Prop2:Spawn()
 
-local Prop3 = Object:new('prop_shuttering04', coords-vec3(0, distance4, .5))
+local Prop3 = Object:new('prop_shuttering04', coords-vec3(0, distance4, .7))
 Prop3:Spawn()
 Prop3:SetRotation(vec3(0, 0, 90))
 
-local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, distance4, -.5))
+local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, distance4, -.7))
 Prop4:Spawn()
 Prop4:SetRotation(vec3(0, 0, 270))
-
-/*
-local Prop5 = Object:new('prop_shuttering04', coords+vec3(0, 0, -1))
-Prop5:Spawn()
-Prop5:SetRotation(vec3(0, 90, 0))
-
-local Prop6 = Object:new('prop_shuttering04', coords+vec3(distance1, 0, -1))
-Prop6:Spawn()
-Prop6:SetRotation(vec3(0, 90, 0))
-
-local Prop7 = Object:new('prop_shuttering04', coords+vec3(distance2, 0, -1))
-Prop7:Spawn()
-Prop7:SetRotation(vec3(0, 90, 0))
-
-local Prop8 = Object:new('prop_shuttering04', coords+vec3(distance3, 0, -1))
-Prop8:Spawn()
-Prop8:SetRotation(vec3(0, 90, 0))
-
-local Prop9 = Object:new('prop_shuttering04', coords-vec3(distance1, 0, 1))
-Prop9:Spawn()
-Prop9:SetRotation(vec3(0, 90, 0))
-
-local Prop10 = Object:new('prop_shuttering04', coords-vec3(distance2, 0, 1))
-Prop10:Spawn()
-Prop10:SetRotation(vec3(0, 90, 0))
-
-local Prop11 = Object:new('prop_shuttering04', coords-vec3(distance3, 0, 1))
-Prop11:Spawn()
-Prop11:SetRotation(vec3(0, 90, 0))
-
-local Prop11 = Object:new('prop_shuttering04', coords-vec3(distance4and2, 0, 1))
-Prop11:Spawn()
-Prop11:SetRotation(vec3(0, 90, 0))
-*/
 
 for distance = -3, 5, 1.2 do
     local Prop_floor= Object:new('prop_shuttering04', coords-vec3(distance, 0, 1))
