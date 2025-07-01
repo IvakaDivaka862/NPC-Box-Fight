e2f645b25a4ecd93d6baf05b7b7cf78adab2b4e3
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-01 22:43:02 +0300

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -39,11 +39,13 @@ end
 
 ---- Fighting Ped
 local ped = Ped:new('a_m_m_golfer_01', coords-vec3(distance3, 0 , 1))
+/*
 local groupa = joaat('group-a')
 local groupb = joaat('group-b') 
 ped:Spawn()
 SetRelationshipBetweenGroups(5, CreateGroup(groupa), CreateGroup(groupb))
 SetPedRelationshipGroupHash(ped.handle, groupa)
+*/
 SetPedRelationshipGroupHash(LocalPlayer.handle, groupb)
 
 
