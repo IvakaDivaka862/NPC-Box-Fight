318c682831cad7587fd75e2590929b325928d6aa
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-01 21:44:20 +0300

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -1,27 +1,32 @@
 local Object = require('@esx_lib/imports/client/entity/object')
 local LocalPlayer = require('@esx_lib/imports/client/entity/ped/localplayer')
 RegisterCommand('start_fight', function()
-local distance = 4
+local distance2 = 2
+local distance4 = 4
 
 local coords = LocalPlayer:GetCoords()
-local Prop1 = Object:new('prop_shuttering04', coords+vec3(distance, 0, -.5))
+local Prop1 = Object:new('prop_shuttering04', coords+vec3(distance4, 0, -.5))
 Prop1:Spawn()
 Prop1:SetRotation(vec3(0, 0, 180))
 
-local Prop2 = Object:new('prop_shuttering04', coords-vec3(distance, 0, .5))
+local Prop2 = Object:new('prop_shuttering04', coords-vec3(distance4, 0, .5))
 Prop2:Spawn()
 
-local Prop3 = Object:new('prop_shuttering04', coords-vec3(0, distance, .5))
+local Prop3 = Object:new('prop_shuttering04', coords-vec3(0, distance4, .5))
 Prop3:Spawn()
 Prop3:SetRotation(vec3(0, 0, 90))
 
-local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, distance, -.5))
+local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, distance4, -.5))
 Prop4:Spawn()
 Prop4:SetRotation(vec3(0, 0, 270))
 
 local Prop5 = Object:new('prop_shuttering04', coords+vec3(0, 0, -1))
 Prop5:Spawn()
 Prop5:SetRotation(vec3(0, 90, 0))
+
+local Prop6 = Object:new('prop_shuttering04', coords+vec3(0, distance2, -1))
+Prop6:Spawn()
+Prop6:SetRotation(vec3(0, 90, 0))
 
 end, false)
 
