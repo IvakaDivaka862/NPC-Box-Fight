87acb3c272f17d6ea3760140ee8fded5910cb4e7
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-01 19:13:50 +0300

    NPC Box Fight 2
    NPC Box Fight

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,10 @@ game 'gta5'
 
 fx_version 'cerulean'
 
-client_script 'client.lua'
-
 dependency 'esx_lib'
 
 shared_script '@esx_lib/imports/shared/package.lua'
+
+file 'client/**'
+
+client_script 'client/client.lua'
