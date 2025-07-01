3b7eadc86d89e797542a424d3c786ced4c50c46b
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-01 20:27:57 +0300

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -1,21 +1,21 @@
 local Object = require('@esx_lib/imports/client/entity/object')
 local LocalPlayer = require('@esx_lib/imports/client/entity/ped/localplayer')
 RegisterCommand('start_fight', function()
-
+local distance = 4
 
 local coords = LocalPlayer:GetCoords()
-local Prop1 = Object:new('prop_shuttering04', coords+vec3(5, 0, 0))
+local Prop1 = Object:new('prop_shuttering04', coords+vec3(distance, 0, -.5))
 Prop1:Spawn()
 Prop1:SetRotation(vec3(0, 0, 180))
 
-local Prop2 = Object:new('prop_shuttering04', coords-vec3(5, 0, 0))
+local Prop2 = Object:new('prop_shuttering04', coords-vec3(distance, 0, -.5))
 Prop2:Spawn()
 
-local Prop3 = Object:new('prop_shuttering04', coords-vec3(0, 5, 0))
+local Prop3 = Object:new('prop_shuttering04', coords-vec3(0, distance, -.5))
 Prop3:Spawn()
 Prop3:SetRotation(vec3(0, 0, 90))
 
-local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, 5, 0))
+local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, distance, -.5))
 Prop4:Spawn()
 Prop4:SetRotation(vec3(0, 0, 270))
 
