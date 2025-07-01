4dd6f050cc66ce92100d73d8fad09055a3a863ca
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-01 21:37:56 +0300

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -19,8 +19,9 @@ local Prop4 = Object:new('prop_shuttering04', coords+vec3(0, distance, -.5))
 Prop4:Spawn()
 Prop4:SetRotation(vec3(0, 0, 270))
 
-local Prop5 = Object:new('prop_shuttering04', coords+vec3(0, 0, -.1))
+local Prop5 = Object:new('prop_shuttering04', coords-vec3(0, 0, -.1))
 Prop5:Spawn()
+Prop5:SetRotation(vec3(0, 0, 90))
 
 end, false)
 
